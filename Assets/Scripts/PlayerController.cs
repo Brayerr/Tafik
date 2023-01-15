dbf86138d3e567c9cdfc2ca21dad03f20ab43433
Brayerr <brayer_alon@walla.com>
2023-01-15 17:43:39 +0200

    initial build mode added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,27 +7,58 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
     Vector2 move;
+    Vector3 direction;
+    [SerializeField] bool buildMode = false;
+
+
+    public delegate void BuildModeToggle();
+
 
 
     private void Start()
     {
-
+        BuildModeToggle buildModeToggle = new(BuildModeToggler);
     }
 
     private void Update()
     {
-        MovePlayer();
-    }
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        move = context.ReadValue<Vector2>();
+        if (!buildMode) MovePlayer();
+        else
+        {
+            AutoMoveDirectionSetter();
+            AutoMovePlayer();
+        }
     }
 
-    void MovePlayer()
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (!buildMode) move = context.ReadValue<Vector2>();
+    }
+
+    public void MovePlayer()
     {
         Vector3 movement = new(move.x, 0f, move.y);
 
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    void AutoMoveDirectionSetter()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) direction = new(0, 0, 1);
+        if (Input.GetKeyDown(KeyCode.S)) direction = new(0, 0, -1);
+        if (Input.GetKeyDown(KeyCode.A)) direction = new(-1, 0, 0);
+        if (Input.GetKeyDown(KeyCode.D)) direction = new(1, 0, 0);
+    }
+
+    public void AutoMovePlayer()
+    {
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    }
+
+    public void BuildModeToggler()
+    {
+        if (buildMode) buildMode = false;
+        else if (!buildMode) buildMode = true;
     }
 
 
