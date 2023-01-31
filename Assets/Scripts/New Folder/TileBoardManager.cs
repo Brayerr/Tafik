d2fa7bf98245c307982e9dd68cc127344a1d763e
Ellie2405 <mttne2405@gmail.com>
2023-01-31 16:30:35 +0200

    Update TileBoardManager.cs

--- Assets/Scripts/New Folder/TileBoardManager.cs
+++ Assets/Scripts/New Folder/TileBoardManager.cs
@@ -10,6 +10,9 @@ public class TileBoardManager : MonoBehaviour
 
     [SerializeField] GameObject EmptyPrefabObject;
     [SerializeField] GameObject FilledPrefabObject;
+    [SerializeField] Sprite[] gridSprites = new Sprite[4096];
+    Vector3 tileRot = new(90, 0, 0);
+
 
     static public TileBoardLogic Board { get; private set; }
     GameObject[,] _tileGraphics;
@@ -29,10 +32,10 @@ public class TileBoardManager : MonoBehaviour
             for (int j = 0; j < dimensions.x; j++)
             {
                 if (Board.Tiles[j, i].State == 0)
-                    _tileGraphics[j, i] = Instantiate(EmptyPrefabObject, new Vector3((j + 1) * space - (space / 2), 0, (i + 1) * space - (space / 2)), Quaternion.identity, this.transform);
+                    _tileGraphics[j, i] = Instantiate(EmptyPrefabObject, new Vector3((j + 1) * space - (space / 2), 1, (i + 1) * space - (space / 2)), Quaternion.identity, this.transform);
                 else
                 {
-                    _tileGraphics[j, i] = Instantiate(FilledPrefabObject, new Vector3((j + 1) * space - (space / 2), 0, (i + 1) * space - (space / 2)), Quaternion.identity, this.transform);
+                    _tileGraphics[j, i] = Instantiate(FilledPrefabObject, new Vector3((j + 1) * space - (space / 2), 0.5f, (i + 1) * space - (space / 2)), Quaternion.identity, this.transform);
                 }
             }
         }
@@ -41,6 +44,27 @@ public class TileBoardManager : MonoBehaviour
 
         TileBoardLogic.OnConvert += DrawFill;
     }
+
+    [ContextMenu("Initialize sprites in Grid")]
+    public void AddSpritesToGrid()
+    {
+        Object[] sprite = Resources.LoadAll($"substance/sand_V2_graph_0/sand_V2_basecolor");
+        int count = 1;
+        //for (int i = 0; i < gridSprites.Length; i++)
+        //{
+        //    gridSprites[i] = sprite[i + 1] as Sprite;
+        //}
+
+        foreach (var item in _tileGraphics)
+        {
+            SpriteRenderer _spriteRenderer = item.GetComponent<SpriteRenderer>();
+            _spriteRenderer.sprite = sprite[count] as Sprite;
+            _spriteRenderer.size = new Vector2(1f, 1f);
+            item.transform.Rotate(tileRot);
+            count++;
+        }
+    }
+
 
     void StartDrawTrail()
     {
@@ -64,7 +88,7 @@ public class TileBoardManager : MonoBehaviour
             for (int j = 0; j < dimensions.x; j++)
             {
                 if (Board.Tiles[j, i].State == 1)
-                    _tileGraphics[j, i].transform.position=new Vector3(j+0.5f,0.5f,i+0.5f);
+                    _tileGraphics[j, i].transform.position = new Vector3(j + 0.5f, 0.5f, i + 0.5f);
             }
         }
     }
