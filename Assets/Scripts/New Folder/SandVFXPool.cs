32bc3dc1235021cdea454f3b276e7aa0cec007d1
Brayerr <brayer_alon@walla.com>
2023-04-12 11:43:00 +0300

    sandVFX update

--- Assets/Scripts/New Folder/SandVFXPool.cs
+++ Assets/Scripts/New Folder/SandVFXPool.cs
@@ -6,17 +6,45 @@ public class SandVFXPool : MonoBehaviour
 {
     public List<GameObject> pooledObjects = new List<GameObject>();
     GameObject objectToPool;
+    GameObject objectToPool1;
+    GameObject objectToPool2;
     [SerializeField] int amountToPool;
     [SerializeField] PlayerLogic player;
-
+    
     private void Start()
     {
+        
         objectToPool = Resources.Load<GameObject>("Prefabs/SandVFX");
+        objectToPool1 = Resources.Load<GameObject>("Prefabs/SandVFX2");
+        objectToPool2 = Resources.Load<GameObject>("Prefabs/SandVFX3");
 
         GameObject sandVFX;
         for (int i = 0; i < amountToPool; i++)
         {
-            sandVFX = Instantiate(objectToPool);
+            int rand = Random.Range(1, 4);
+            switch (rand)
+            {
+                case 1:
+                    {
+                        sandVFX = Instantiate(objectToPool);
+                        break;
+                    }
+                case 2:
+                    {
+                        sandVFX = Instantiate(objectToPool1);
+                        break;
+                    }
+                case 3:
+                    {
+                        sandVFX = Instantiate(objectToPool2);
+                        break;
+                    }
+                default:
+                    {
+                        sandVFX = Instantiate(objectToPool2);
+                        break;
+                    }
+            }    
             sandVFX.gameObject.SetActive(false);
             pooledObjects.Add(sandVFX);
         }
